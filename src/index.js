4ebce2fbd92e95f73444c93fc5f40294a4866021
chicco4 <chiccotto4@gmail.com>
2022-07-08 15:47:15 +0200

    basic skeleton

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
-console.log("test")
+import "./styles/global.css";
+
+console.log("test");
+
+/* HEADER */
+
+function createHeader() {
+  const header = document.createElement("header");
+
+  const pageTitle = document.createElement("h1");
+  pageTitle.textContent = "Weather-app";
+
+  header.appendChild(pageTitle);
+
+  return header;
+}
+
+/* MAIN */
+
+function createMain() {
+  const main = document.createElement("main");
+  main.textContent = "main";
+  return main;
+}
+
+/* FOOTER */
+
+function createFooter() {
+  const footer = document.createElement("footer");
+  footer.classList.add("footer");
+
+  const copyright = document.createElement("p");
+  copyright.textContent = `Copyright © ${new Date().getFullYear()}`;
+
+  const githubLink = document.createElement("a");
+  githubLink.href = "https://github.com/chicco4";
+  githubLink.textContent = "chicco4";
+
+  footer.appendChild(copyright);
+  footer.appendChild(githubLink);
+
+  return footer;
+}
+
+/* START */
+
+function initializeContent() {
+  document.body.appendChild(createHeader());
+  document.body.appendChild(createMain());
+  document.body.appendChild(createFooter());
+}
+
+initializeContent();
